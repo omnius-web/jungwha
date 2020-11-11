d7904218d35c84bb9242fa5ed4cb2136772b74d2
omnius <gs5m3011@gmail.com>
2020-11-11 10:24:25 +0900

    이름 휴대폰번호 *변환

---
+++ models/namehpsecu.js
@@ -0,0 +1,18 @@
+module.exports = function(strName, hp){
+    var namehp = {};
+    if (strName.length > 2) {
+        var originName = strName.split('');
+        originName.forEach(function(name, i) {
+        if (i === 0 || i === originName.length - 1) return;
+        originName[i] = '*';
+        });
+        var joinName = originName.join();
+        namehp.name = joinName.replace(/,/g, '');
+    } else {
+        var pattern = /.$/; // 정규식
+        namehp.name = strName.replace(pattern, '*');
+    }
+    namehp.hp = hp.replace(  /(\d{3})(\d{4})(\d{4})/,  "$1****$3"  );
+
+    return namehp;
+}
